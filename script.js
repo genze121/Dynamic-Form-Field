462386bc49e1484a5fad94512f65199de6e7f5b2
Tirtha Sharma <tirthagenze121@gmail.com>
2024-07-15 21:43:38 +0530

    Removed the commented code.

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ function addFormFields() {
   const inputElement = document.createElement("input");
   inputElement.classList.add("input-element");
   inputElement.placeholder = "Enter Form Field";
-  // mainContainer.style.height = "800px";
   wrapperContainer.appendChild(inputElement);
 }
 
 function removeFormFields() {
   const removeElement = document.querySelector(".input-element");
-  // mainContainer.style.height = "300px";
   wrapperContainer.removeChild(removeElement);
 }
 
